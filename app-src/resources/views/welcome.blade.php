3a88ed55980e076186bb3b9b3942f400066f7154
Xin-an <f107110126@nkust.edu.tw>
2019-05-26 02:22:35 +0800

    test include css directly successfully

--- app-src/resources/views/welcome.blade.php
+++ app-src/resources/views/welcome.blade.php
@@ -9,6 +9,8 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+    <link href="css/my.css" rel="stylesheet">
+
     <!-- Styles -->
     <style>
         html, body {
