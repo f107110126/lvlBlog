ec65f1fbf9cc65c9e0260855b4dc8b5b66e1e9ed
Andy.Chen <f107110126@gmail.com>
2020-03-07 21:46:10 +0800

    update reset password email

--- app-src/app/Admin/Admin.php
+++ app-src/app/Admin/Admin.php
@@ -3,8 +3,18 @@
 namespace App\Admin;
 
 use App\User;
+use App\Admin\Auth\ResetPassword as ResetPasswordNotification;
 
 class Admin extends User
 {
-    //
+    /**
+     * Send the password reset notification.
+     *
+     * @param string $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPasswordNotification($token));
+    }
 }
