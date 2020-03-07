c66194ff125accaee2b4973a1d324d1a2b7c8f28
Andy.Chen <f107110126@gmail.com>
2020-03-07 21:35:32 +0800

    separate logout effective

--- app-src/app/Http/Controllers/Client/Auth/LoginController.php
+++ app-src/app/Http/Controllers/Client/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Client\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
@@ -29,6 +30,21 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest:member')->except('logout');
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->regenerateToken();
+
+        return $this->loggedOut($request) ?: redirect('/');
     }
 
     /**
