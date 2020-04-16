b18a7aeaa1c887b2571994a718cf3ff4b0b0f937
Andy.Chen <f107110126@gmail.com>
2020-04-16 11:19:07 +0800

    update api

--- app-src/routes/api.php
+++ app-src/routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:member')->get('/member', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:admin')->get('/admin', function (Request $request) {
     return $request->user();
 });
